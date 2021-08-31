319a5b5e5083bcf34156ad5b2e91db2d294278cb
Emil Romanov <evr1ka@mail.ru>
2021-08-31 13:59:01 +0300

    Initial commit

---
+++ DisableAntivirusMS.ps1
@@ -0,0 +1,8 @@
+﻿$antivStatus = get-MpPreference
+if($antivStatus.DisableRealtimeMonitoring -eq $false){
+    Set-MpPreference -DisableRealtimeMonitoring $true
+    Write-host "Antivirus was disabled" -f Green
+}
+else{
+Write-host "Antivirus was disabled earlier" -f Yellow
+}
